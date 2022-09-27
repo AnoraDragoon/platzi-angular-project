bd19842aa3f12316e739ca2699be42c919bfc4ea
AnoraDragoon <anoradragoon@gmail.com>
2022-09-28 00:28:26 +0200

    implment product detail

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -15,6 +15,7 @@ export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  showProductDetail: boolean = false;
 
   constructor(
     private storeService: StoreService,
@@ -25,14 +26,24 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.productsService.getAllProducts()
-    .subscribe(data => {
-      this.products = data;
-    });
+      .subscribe(data => {
+        this.products = data;
+      });
   }
 
   onAddToShoppingCart(product: Product) {
     this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
+  }
+
+  toggleProductDetail() {
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  onShowDetail(id:string) {
+    this.productsService.getProduct(id).subscribe(data => {
+      console.log(data);
+    })
   }
 
 }
