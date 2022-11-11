f34970f0d36c9aa6b15ee48f7e9e70ce03e073d6
Pérez González, Osvaldo <osvaldo.perezgonzalez@sicpa.com>
2022-11-11 22:08:55 +0100

    Implement update product
    use put http method to update

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { CreateProductDTO, Product } from './../models/product.model';
+import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private readonly apiUrl= 'https://young-sands-07814.herokuapp.com/api/products';
+  private readonly apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
 
   constructor(
     private http: HttpClient
@@ -18,11 +18,15 @@ export class ProductsService {
     return this.http.get<Product[]>(this.apiUrl);
   }
 
-  getProduct(id:string) {
+  getProduct(id: string) {
     return this.http.get<Product>(`${this.apiUrl}/${id}`);
   }
 
   create(dto: CreateProductDTO) {
     return this.http.post<Product>(this.apiUrl, dto);
+  }
+
+  update(id: string, dto: UpdateProductDTO) {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, dto);
   }
 }
