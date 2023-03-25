1bc573cbf4c698a3f3d796f090e335bae1b2d73d
AnoraDragoon <anoradragoon@gmail.com>
2023-03-25 13:05:10 +0100

    Project proxy - class 11
    Implement proxy config to solve CORS issues in development

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,7 +8,7 @@ import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product
 })
 export class ProductsService {
 
-  private readonly apiUrl = 'https://young-sands-07814.herokuapp.com/api/products';
+  private readonly apiUrl = '/api/products';
 
   constructor(
     private http: HttpClient
