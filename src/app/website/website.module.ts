0b279f422368d1672d180354b6dfb0972cd8bcfe
Pérez González, Osvaldo <osvaldo.perezgonzalez@sicpa.com>
2023-06-04 12:42:28 +0200

    Creando el CMS Module (13/25) y Creando en Website Module (14/25)
    cms and website modules are imported on AppRoutingModule using lazy loading

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -1,14 +1,51 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SwiperModule } from 'swiper/angular';
 
 import { WebsiteRoutingModule } from './website-routing.module';
+import { ImgComponent } from './components/img/img.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { CategoryComponent } from './pages/category/category.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective,
+    HomeComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
+    ProductDetailComponent,
+    LayoutComponent
+  ],
   imports: [
     CommonModule,
-    WebsiteRoutingModule
+    WebsiteRoutingModule,
+    FormsModule,
+    SwiperModule
   ]
 })
 export class WebsiteModule { }
